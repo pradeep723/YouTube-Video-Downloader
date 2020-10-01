89470e6735b4f1c03962bcf38e874351d9a79942
Aman Ojha <alexmercerr07@gmail.com>
2020-10-01 12:50:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,26 @@ class y2down():
         print('|--------------------------------------------------------|')
         y2down.getTheLink('Get the Link')
 
+    def getTheLink(self):
+        videoURL = str(input('Enter the URL to Download the Video : '))
+        download = pytube.YouTube(videoURL)
+        print(download)
+        downloaded = y2down.downloadthis('Download this video', download)
+        if downloaded:
+            option = str(
+                input('Want to Download More YouTube Videos (Y/N) : '))
+            if option == 'Y':
+                main()
+                exit()
+            elif option == 'N':
+                print('Happy to Serve You Please Use Our Service More')
+                exit()
+            else:
+                print(
+                    'Please Enter the Correct Option.\n\nCurrently Quiting Better Luck Next time')
+                exit()
+
+
 def main():
     y2down.view('get the link')
 
